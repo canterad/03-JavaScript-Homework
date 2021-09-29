d45b5b284e1b19de3854b91e4d42cafc84c96383
Duane Cantera <duane.cantera@comcast.net>
2021-09-29 08:56:53 -0400

    I put up all of my files to complete this assignment.

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # 03-JavaScript-Homework
 This Repository contains all of the files for Boot Camp Home work Assignment #3
+
+Developer: Duane Cantera
+Date: Oct. 2, 2021
+Assignment: 03 JavaScript: Password Generator
+
+*******************
+CODE DESIGN ISSUES:
+*******************
+
+Issue: Found that I was alerting the user with a lot of error messages.
+Solution: I decided to use a boolean variable and set its initial value to true.  Instead of writing out an error message when an error occurred I set the variable to false instead.  I structured my code with series of if - else conditions so the path would only continue if the variable was still true, if it was still true I kept on testing my error conditions.  After I was done testing all my error conditions I then tested the state of the variable so I only had to display one error condition if any of the error conditions occurred.
+
+Issue: Testing if a numeric value was entered for the password size.
+Solution: I decided to deal with this by looping through the string that contained the password size and tested each individual character to test if it is not a numeric character.  I did this by testing if it is < the character '0' or > the character '9'.  If any of the characters were not numeric characters then I set an error condition and exited the loop.  After exiting the loop if the error condition was not set then I knew I could safely convert the string to an integer by calling the parseInt function.
+
+Issue: How to generate the password.
+Solution:  I create an empty string called szPassword that would hold the generated password.  I had a password type string set as a empty string.  I created constant strings for the character types, upper case, lower case, numeric and special characters.  Based on the users response I then use the concat function to add the constant strings to the password type string.  I looped through a for loop using the Password Size as an index.  For each time through I called the Math.random function to generated a random float value from zero to the PasswordTypes string length, and then rounded it to its nearest whole number by calling the Math.floor function.  I used the whole number returned as the index into the Password Type string and added the character at that position to the szPassword string.  I returned the szPassword string as the generated password.
+
+***************
+LINKS:
+***************
+
+Link To Deployed Application: https://canterad.github.io/03-JavaScript-Homework/
--- script.js
+++ script.js
@@ -1,14 +1,199 @@
-// Assignment Code
+// ************************************************************************************************************************************
+// Assignment Code.  Get the first element in the document with id value of "generate".
+// ************************************************************************************************************************************
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// **************************************************************************************************************************************
+// Use the generate button that was retrieved above to add an event listener for the generate button.  The addEventListener is listening 
+// for a click event for the "generate" button and it then calls the writePassword function.
+// **************************************************************************************************************************************
+generateBtn.addEventListener("click", writePassword);
 
+
+// **************************************************************************************************************************************
+// Function: writePassword: Write password to the #password input.  The function "generatePassword" is called.  The return string
+// value is stored in the variable password.  A call is made to querySelector to get the first element in the document with the id value
+// of "password".  The password elements value property is set to the password value. 
+// **************************************************************************************************************************************
+function writePassword() 
+{
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
+   
   passwordText.value = password;
-
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// ***************************************************************************************************************************************
+// Function: generatePassword
+//
+// Set the initial value of the variable isPasswordSizeValid to true;
+//
+// Prompt the user asking the user to enter the size of the password.  If the user selected the cancel button the return value is null.
+//
+// Test if the return value is null, if it is then set the variable isPasswordSizeValid to false.
+//
+// Else - Perform the following operations:
+//
+//    Test the length of the string returned. If it is zero then set the variable isPasswordSizeValid to false.
+//
+//        Else - Perform the following operations:
+//
+//            Loop through the return string. 
+//                if the character is not numeric.  Perform the following operations.
+//                    Set the variable isPasswordSizeValid to false.
+//                    Set the index to the size of the string to exit the for loop.
+//
+//            If the variable isPasswordSizeValid is true then perform the following operations:
+//                Convert the string to an integer value.
+//                If the integer value is < 8 or > 128 then set the variable isPasswordSizeValid to false.
+//
+//  If the Password Size value is not valid then do the following operations:
+//      Alert the user that the Password Size value entered is not valid.
+//      Return the Operation Failed message.
+//
+//  Prompt the user for the character types to include in the password.
+//
+//  If no character types were selected then perform the following operations:
+//      Alert the user that they did not select a character type for the password.
+//      Return the Operation Failed message.
+//
+//  if the isLowerCase variable is true then add the lower case string to the szPasswordTypes string.
+//  if the isUpperCase variable is true then add the upper case string to the szPasswordTypes string.
+//  if the isNumeric variable is true then add the numeric string to the szPasswordTypes string.
+//  if the isSpecialCharacter variable is true then add the special characters string to the szPasswordTypes string.
+//
+//  Loop through a for loop using the Password Size as an index and perform the following operations:  
+//      Generate a random float from zero to the PasswordTypes string length. 
+//      Round the float value to its nearest whole number with the Math.floor function.  
+//      Use the whole number returned as the index into the Password Type string. 
+//      Add the character at that position in the PasswordType string to the string szPassword. 
+//
+//  Return the szPassword string which contains the generated password.
+//
+// ***************************************************************************************************************************************
+function generatePassword()
+{
+  // Create constant strings for the different character types and failed message.
+  const szlowerCase = "abcdefghijklmnopqrstuvwxyz";
+  const szUpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const szNumeric = "1234567890";
+  const szSpecialCharacters = " !\"#$%&\'()*+,-./:;<=>?@[\\]^`{|}~";
+  const szOperationFailed = "Operation Failed - Please Try Again";
+  
+  // Create and set default values for variables.
+  let szPassword = "";
+  let isLowerCase = false;
+  let isUpperCase = false;
+  let isNumeric = false;
+  let isSpecialCharacters = false;
+  let nPasswordSize = 0;
+  let szPasswordSize = "";
+  let nIndex = 0;
+  let isPasswordSizeValid = true;
+  let szPasswordTypes = "";
+  let nRandomArrayPosition = 0;
+
+  // Prompt user to enter the size of the password.
+  szPasswordSize = prompt("Please enter a numeric value for the password size.\r\nIt must be between 8 and 128.");
+
+  // If the Cancel button was selected the return value is null so set the isPasswordSizeValid to false.
+  if (szPasswordSize == null) 
+  {
+    isPasswordSizeValid = false;
+  }
+  else
+  {
+    // If the length of the string is zero then set the isPasswordSizeValid to false.
+    if (szPasswordSize.length === 0)
+    {
+      isPasswordSizeValid = false;    
+    }
+    else
+    {
+      // Loop through the string characters and test if numeric, if not set the isPasswordSize valid to false
+      // and set the nIndex value equal to the szPasswordSize length to exit the for loop.      
+      for (nIndex = 0;  nIndex < szPasswordSize.length; nIndex++)
+      {
+        if (szPasswordSize[nIndex] < '0' || szPasswordSize[nIndex] > '9')
+        {
+          isPasswordSizeValid = false;
+          nIndex = szPasswordSize.length;
+        }
+      }
+
+      // If the Password Size string contains all numeric values then convert the string to an integer value.
+      if (isPasswordSizeValid)
+      {
+        // Convert the string to an integer value.
+        nPasswordSize = parseInt(szPasswordSize);
+
+        // If the integer value is less than 8 or greater than 128 it is out of range so
+        // set the isPasswordSizeValid value to false.
+        if (nPasswordSize < 8 || nPasswordSize > 128)
+        {
+          isPasswordSizeValid = false;
+        }
+      }
+    }
+  }
+
+  // If the Password Size value is not valid then alert the user and return passing the Operation Failed message.
+  if (!isPasswordSizeValid)
+  {
+    window.alert("You did not enter a valid value.\r\nThe \"Generate Password\" operation could not be performed.\r\nPlease try again.");
+    return (szOperationFailed);  
+  }
+ 
+  // Prompt the user for the character types:
+  isLowerCase = window.confirm("Should the Password include lower case characters?\r\n\r\nOK = \"Yes\"\r\nCancel = \"No\"");
+  isUpperCase = window.confirm("Should the Password include upper case characters?\r\n\r\nOK = \"Yes\"\r\nCancel = \"No\"");
+  isNumeric = window.confirm("Should the Password include numeric characters?\r\n\r\nOK = \"Yes\"\r\nCancel = \"No\"");
+  isSpecialCharacters = window.confirm("Should the Password include special characters?\r\n\r\nOK = \"Yes\"\r\nCancel = \"No\"");
+
+  // Test if all of these are set to false, if so tell the user that they did not select a character type and return
+  // passing the Operation Failed message.
+  if ((!isLowerCase) && (!isUpperCase) && (!isNumeric) && (!isSpecialCharacters))
+  {
+    window.alert("You did not select any character types for the password.\r\nThe \"Generate Password\" operation could not be performed.\r\nPlease try again.");
+    return (szOperationFailed);      
+  }
+
+  // If the isLowerCase variable is true then add the lower case string to the PasswordTypes string.
+  if (isLowerCase)
+  {
+    szPasswordTypes = szPasswordTypes.concat(szlowerCase);
+  }
+
+  // If the isUpperCase variable is true then add the upper case string to the PasswordTypes string.
+  if (isUpperCase)
+  {
+    szPasswordTypes = szPasswordTypes.concat(szUpperCase);
+  }
+
+  // If the isNumeric variable is true then add the numeric string to the PasswordTypes string.
+  if (isNumeric)
+  {
+    szPasswordTypes = szPasswordTypes.concat(szNumeric);
+  }
+
+  // If the isSpecialCharacters variable is true then add the special characters string to the PasswordTypes string.
+  if (isSpecialCharacters)
+  {
+    szPasswordTypes = szPasswordTypes.concat(szSpecialCharacters);
+  }
+  
+  // Loop through a for loop using the Password Size as an index.  For each time through call the function Math.random to generate 
+  // a random float number with a range from zero to the PasswordTypes string length.  Call the function Math.floor and round the
+  // float value to its nearest whole number.  Use the whole number returned as the index in the Password Type string and 
+  // add the character at that position to the string szPassword.
+  for (nIndex = 0; nIndex < nPasswordSize; nIndex++)
+  {
+    nRandomArrayPosition = Math.floor(Math.random() * szPasswordTypes.length);
+    szPassword += szPasswordTypes[nRandomArrayPosition];
+  }
+  
+  // Return the generated password.
+  return(szPassword);
+}
+
+
